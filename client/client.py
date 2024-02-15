7f7c3b590ff23728e9a1639c01db452d935fa01d
Camille CAMBIUM <royalfr@protonmail.com>
2024-02-15 10:55:28 +0100

    FIX : client.py
    fixed mixed space problemes

--- client/client.py
+++ client/client.py
@@ -8,16 +8,16 @@ import threading
 import time
 
 class style():
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m' 
+	BLACK = '\033[30m'
+	RED = '\033[31m'
+	GREEN = '\033[32m'
+	YELLOW = '\033[33m'
+	BLUE = '\033[34m'
+	MAGENTA = '\033[35m'
+	CYAN = '\033[36m'
+	WHITE = '\033[37m'
+	UNDERLINE = '\033[4m'
+	RESET = '\033[0m' 
 
 buffer = ""
 current_name = ""
@@ -215,7 +215,7 @@ def main():
 			print("\r", end="")
 			running = False
 			break
-		
+
 		print("\r", end="")
 		for fd, event in poll_state:
 			if fd == client_socket.fileno():
